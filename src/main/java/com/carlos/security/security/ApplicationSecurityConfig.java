e3d25585208fcadc80cbcc347b3fd2dacec40530
Carlos Dominguez Puig <carlos.dom13@hotmail.com>
2020-06-05 04:50:56 +0200

    primer commit, preAuthorize

---
+++ src/main/java/com/carlos/security/security/ApplicationSecurityConfig.java
@@ -0,0 +1,72 @@
+package com.carlos.security.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.carlos.security.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.carlos.security.security.ApplicationUserRole.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+  private final PasswordEncoder passwordEncoder;
+  @Autowired
+  public ApplicationSecurityConfig(PasswordEncoder passwordEncoder){
+    this.passwordEncoder=passwordEncoder;
+  }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+        .antMatchers("/api/**").hasRole(STUDENT.name())
+//        .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//        .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//        .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//        .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+//        los antmatches has sido sustituidos por anotaciones en
+//        el StudentManagementController y @EnableGlobalMethodSecurity(prePostEnabled = true)
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic();
+  }
+
+  @Override
+  @Bean
+  protected UserDetailsService userDetailsService() {
+    UserDetails annaSmithUser = User.builder()
+        .username("annasmith")
+        .password(passwordEncoder.encode("password"))
+        .roles(STUDENT.name())
+        .authorities(STUDENT.getGrantedAuthorities())
+        .build();
+    UserDetails lindaUser = User.builder()
+        .username("linda")
+        .password(passwordEncoder.encode("password123"))
+        .roles(ADMIN.name())
+        .authorities(ADMIN.getGrantedAuthorities())
+        .build();
+    UserDetails tomUser = User.builder()
+        .username("tom")
+        .password(passwordEncoder.encode("password123"))
+        .roles(ADMINTRAINEE.name())
+        .authorities(ADMINTRAINEE.getGrantedAuthorities())
+        .build();
+    return new InMemoryUserDetailsManager(
+      annaSmithUser, lindaUser, tomUser
+    );
+  }
+}
